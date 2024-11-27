64bd3196281ac783b6e733bb365493d4e6a45a4e
dayy210 <dddy0210@naver.com>
2024-11-27 10:36:59 +0900

    Add files via upload

---
+++ JAVA/Ex_0821/src/ex_04/Ttt.java
@@ -0,0 +1,17 @@
+package ex_04;
+
+public class Ttt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int x = 5;
+		int y = x++;
+
+		System.out.println("x의 값 : " +x); // x : ?
+		System.out.println("y의 값 : " +y); // y : ?
+	}
+
+}
